e215686bedba7f4d5ff48014722861aa582f5f8c
Michelle Levine <michelle@michellejl.com>
2018-06-26 10:59:46 -0700

    move nav links from each page to nav components

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Nav from './nav'
+
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+  <div>
+    <div>
+      <h1>
+        <Link to="/" >
           {siteTitle}
         </Link>
       </h1>
     </div>
+    <Nav />
   </div>
 )
 
--- src/pages/community.js
+++ src/pages/community.js
@@ -1,32 +1,10 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
 const CommunityPage = () => (
   <Layout>
     <h1>Community</h1>
-    <h2>Pages</h2>
-    <Link to="/">home</Link>
-    <br />
-    <Link to="/about/">about</Link>
-    <br />
-    <Link to="/code/">coding</Link>
-    <br />
-    <Link to="/blog/">blog</Link>
-    <br />
-    <Link to="/photo/">photography</Link>
-    <br />
-    <Link to="/resources/">resources</Link>
-    <br />
-    <Link to="/uses/">uses</Link>
-    <br />
-    <Link to="/now/">now</Link>
-    <br />
-    <Link to="/community/">community</Link>
-    <br />
-    <Link to="/resume/">resume</Link>
-    <br />
   </Layout>
 )
 
